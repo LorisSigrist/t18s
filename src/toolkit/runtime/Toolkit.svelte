b6995a3babf3f88dcd90b75cf7dae5da962cbbda
Loris Sigrist <sigrist@hasch.io>
2023-10-04 15:18:02 +0200

    Confirm that $t18s is accessible in toolkit

--- src/toolkit/runtime/Toolkit.svelte
+++ src/toolkit/runtime/Toolkit.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
   // @ts-expect-error missing
-import Icon from 'virtual:t18s-toolkit:Icon.svelte';
-
+  import Icon from "virtual:t18s-toolkit:Icon.svelte";
+  import { locales, locale } from "$t18s";
 
   function test() {
-      if(import.meta.hot) {
-        import.meta.hot.send("t18s:test", {});
-      }
+    if (import.meta.hot) {
+      import.meta.hot.send("t18s:test", {});
+    }
   }
 </script>
 
-
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="t18s-toolkit" on:click={test}>
-  <Icon/>
+  <Icon />
 </div>
 
 <style>
@@ -25,7 +24,7 @@ import Icon from 'virtual:t18s-toolkit:Icon.svelte';
 
     font-size: 1.5rem;
 
-    padding: .5rem;
+    padding: 0.5rem;
     background-color: #ff3e00;
     color: white;
     border-radius: 0.25rem;
