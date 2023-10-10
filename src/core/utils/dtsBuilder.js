5610620c6b90b2bf3b68e847af6aeb01a7ffeeb2
Loris Sigrist <sigrist@hasch.io>
2023-10-10 14:53:12 +0200

    Use dts builder

---
+++ src/core/utils/dtsBuilder.js
@@ -0,0 +1,140 @@
+export class DTSBuilder {
+  /** @type {Set<Module>} */
+  #modules = new Set();
+
+  /** @type {string | undefined} */
+  #disclaimer = undefined;
+
+  /**
+   * @param {string} name
+   * @param {(module : Module) => void} moduleBuilder
+   */
+  addModule(name, moduleBuilder) {
+    const module = new Module(name);
+    moduleBuilder(module);
+    this.#modules.add(module);
+    return this;
+  }
+
+  /** @param {string | undefined} disclaimer */
+  setDisclaimer(disclaimer) {
+    this.#disclaimer = disclaimer;
+    return this;
+  }
+
+  build() {
+    let code = "";
+    if (this.#disclaimer) {
+      const lines = this.#disclaimer.split("\n");
+      for (const line of lines) {
+        code += `// ${line}\n`;
+      }
+
+      code += "\n";
+    }
+
+    for (const module of this.#modules) {
+      code += module.build();
+    }
+
+    return code;
+  }
+}
+
+class Module {
+  /** @type {string} */
+  #name;
+
+  /** @type {string | undefined} */
+  #description = undefined;
+
+  /** @type {Statement[]} */
+  #statements = [];
+
+  /** @param {string} name */
+  constructor(name) {
+    this.#name = name;
+  }
+
+  /** @param {string | undefined} description */
+  setDescription(description) {
+    this.#description = description;
+    return this;
+  }
+
+  /**
+   * @param {string} code
+   * @param {((statement : Statement) => void) | undefined} statementBuilder
+   */
+  addStatement(code, statementBuilder = undefined) {
+    const statement = new Statement(code);
+    if (statementBuilder) {
+      statementBuilder(statement);
+    }
+
+    this.#statements.push(statement);
+    return this;
+  }
+
+  build() {
+    let code = "";
+
+    if (this.#description) {
+      code += "/**\n";
+      const lines = this.#description.split("\n");
+      for (const line of lines) {
+        code += ` * ${line}\n`;
+      }
+      code += " */\n";
+    }
+
+    code += `declare module "${this.#name}" {\n`;
+    code += indent(this.#statements.map((s) => s.build()).join("\n\n"));
+    code += "\n}";
+    return code;
+  }
+}
+
+class Statement {
+  /** @type {string} */
+  #code;
+
+  /** @type {string | undefined} */
+  #description = undefined;
+
+  /** @param {string} code */
+  constructor(code) {
+    this.#code = code;
+  }
+
+  /** @param {string|undefined} description */
+  setDescription(description) {
+    this.#description = description;
+    return this;
+  }
+
+  build() {
+    let code = "";
+    if (this.#description) {
+      code += "/**\n";
+      const lines = this.#description.split("\n");
+      for (const line of lines) {
+        code += ` * ${line}\n`;
+      }
+      code += " */\n";
+    }
+
+    code += this.#code;
+    return code;
+  }
+}
+
+/**
+ * Indents some text by one level.
+ * @param {string} text
+ */
+function indent(text) {
+  const INDENT_SPACES = 4;
+  const lines = text.split("\n");
+  return lines.map((l) => " ".repeat(INDENT_SPACES) + l).join("\n");
+}
