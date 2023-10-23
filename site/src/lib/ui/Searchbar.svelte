1daded6889b370564c6f86f7505deadfb6b49003
Loris Sigrist <sigrist@hasch.io>
2023-10-23 21:03:18 +0200

    Better Error Handling

--- site/src/lib/ui/Searchbar.svelte
+++ site/src/lib/ui/Searchbar.svelte
@@ -1,11 +1,17 @@
 <script>
-    import SearchIcon from "virtual:icons/heroicons/magnifying-glass";
-    export let query = "";
+  import SearchIcon from "virtual:icons/heroicons/magnifying-glass";
+  export let query = "";
 </script>
 
-
-<label class="flex gap-2 bg-gray-100 text-gray-800 pl-4 rounded-md items-center focus-within:ring-2 ring-orange-600">
-    <SearchIcon class="text-gray-500 w-4 h-4 min-w-max" />
-    <span class="sr-only">Search</span>
-    <input type="text" bind:value={query} class="bg-transparent py-2 pe-2 focus-within:outline-none" />
+<label
+  class="flex gap-2 bg-gray-100 text-gray-800 pl-4 rounded-md items-center focus-within:ring-2 ring-orange-600"
+>
+  <SearchIcon class="text-gray-400 w-4 h-4 min-w-max" />
+  <span class="sr-only">Search</span>
+  <input
+    type="text"
+    bind:value={query}
+    class="bg-transparent py-2 pe-2 focus-within:outline-none placeholder:text-gray-400"
+    placeholder="Search"
+  />
 </label>
---
+++ site/src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page } from "$app/stores";
+  import NotFoundPage from "./404/NotFoundPage.svelte";
+</script>
+
+{#if $page.status === 404}
+  <NotFoundPage />
+{:else}
+  <h1>Something went wrong {$page.status}</h1>
+{/if}
