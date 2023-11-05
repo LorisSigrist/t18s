24d5b4bbc9d1d4b581e04900bbcce30e4e2eed35
LorisSigrist <43482866+LorisSigrist@users.noreply.github.com>
2023-11-05 20:36:32 +0000

    Deploying to gh-pages from @ LorisSigrist/t18s@38e8ba0bc4152b2da049dffeb66544f6b0b45927 🚀

--- _app/immutable/chunks/_t18s-dictionary_en_messages.fa3ec860.js
+++
@@ -1,1 +0,0 @@
-const e={hello:"Hello World","language.en":"English","language.de":"German","link.home":"Home"};export{e as default};
---
+++ _app/immutable/chunks/_t18s.298bc31f.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index.e94a34b1.js";const s=["en","de"],l="en",o=e(s[0]),t=o.set,n=a=>s.includes(a);export{o as a,l as f,n as i,s as l,t as s};
